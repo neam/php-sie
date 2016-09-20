71a2a50f130ef2e55b3be35e12137f957db1433f
Fredrik Wollsén <fredrik@neam.se>
2016-09-20 22:44:33 +0200

    Added additional build entry test

--- tests/unit/BuildEntryTest.php
+++ tests/unit/BuildEntryTest.php
@@ -20,6 +20,26 @@ class BuildEntryTest extends \Codeception\Test\Unit
 
     // tests
 
+    public function testCall()
+    {
+        $line = '#TRANS 2400 {} -200 20130101 "Foocorp expense"';
+        $tokenizer = new Tokenizer($line);
+        $tokens = $tokenizer->tokenize();
+        $first_token = array_shift($tokens);
+        $buildEntry = new BuildEntry($line, $first_token, $tokens, false);
+        $entry = $buildEntry->call();
+
+        codecept_debug(__METHOD__);
+        codecept_debug($entry);
+
+        $this->assertEquals(2400, $entry->attributes->kontonr);
+        $this->assertEquals(-200, $entry->attributes->belopp);
+        $this->assertEquals(20130101, $entry->attributes->transdat);
+        $this->assertEquals("Foocorp expense", $entry->attributes->transtext);
+        $this->assertFalse(isset($entry->attributes->kvantitet));
+
+    }
+
     public function testCallWithAnUnexpectedTokenAtStartOfArray()
     {
 
